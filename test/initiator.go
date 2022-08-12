9117668cff6dae94bf52e927fe22259f956a5292
yonas kemon <yonaskemon01@gmail.com>
2022-08-12 14:44:50 +0300

    created authenticate function to get access token

--- test/initiator.go
+++ test/initiator.go
@@ -2,23 +2,38 @@ package test
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+	"sso/initiator"
+	"sso/internal/constant/model/db"
+	"sso/internal/constant/model/dto"
+	"sso/internal/handler/middleware"
+	"sso/platform/logger"
+
+	"github.com/casbin/casbin/v2"
+	"github.com/cucumber/godog"
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 	"github.com/spf13/viper"
-	"os"
-	"sso/initiator"
-	"sso/internal/constant/model/db"
-	"sso/internal/handler/middleware"
-	"sso/platform/logger"
+	"gitlab.com/2ftimeplc/2fbackend/bdd-testing-framework/src"
 )
 
 type TestInstance struct {
-	Server *gin.Engine
-	DB     *db.Queries
-	Redis  *redis.Client
-	Module initiator.Module
+	Server   *gin.Engine
+	DB       *db.Queries
+	Redis    *redis.Client
+	Module   initiator.Module
+	User     dto.User
+	response struct {
+		OK   bool              `json:"ok"`
+		Data dto.TokenResponse `json:"data"`
+	}
+	AccessToken string
+	enforcer    *casbin.Enforcer
 }
 
 func Initiate(path string) TestInstance {
@@ -67,7 +82,7 @@ func Initiate(path string) TestInstance {
 	log.Info(context.Background(), "platform layer initialized")
 
 	log.Info(context.Background(), "initializing module")
-	module := initiator.InitModule(persistence, cacheLayer, path+viper.GetString("private_key"), platformLayer, log)
+	module := initiator.InitModule(persistence, cacheLayer, path+viper.GetString("private_key"), platformLayer, log, enforcer)
 	log.Info(context.Background(), "module initialized")
 
 	log.Info(context.Background(), "initializing handler")
@@ -87,13 +102,71 @@ func Initiate(path string) TestInstance {
 
 	log.Info(context.Background(), "initializing router")
 	v1 := server.Group("/v1")
-	initiator.InitRouter(server, v1, handler, module, log, enforcer,  path+viper.GetString("public_key"))
+	initiator.InitRouter(server, v1, handler, module, log, enforcer, path+viper.GetString("public_key"))
 	log.Info(context.Background(), "router initialized")
 
 	return TestInstance{
-		Server: server,
-		DB:     sqlConn,
-		Redis:  cache,
-		Module: module,
+		Server:   server,
+		DB:       sqlConn,
+		Redis:    cache,
+		Module:   module,
+		enforcer: enforcer,
 	}
+}
+func (t *TestInstance) Authenicate(creadentials *godog.Table) error {
+	// read email and password from table
+	apiTest := src.ApiTest{
+		URL:    "/v1/login",
+		Method: http.MethodPost,
+	}
+	email, err := apiTest.ReadCellString(creadentials, "email")
+	if err != nil {
+		return err
+	}
+	password, err := apiTest.ReadCellString(creadentials, "password")
+	if err != nil {
+		return err
+	}
+	hash, err := t.Module.OAuthModule.HashAndSalt(context.Background(), []byte(password))
+	if err != nil {
+		return err
+	}
+	userData, err := t.DB.CreateUser(context.Background(), db.CreateUserParams{
+		Email: sql.NullString{
+			String: email,
+			Valid:  true,
+		},
+		Password: hash,
+	})
+	if err != nil {
+		return err
+	}
+
+	t.User.ID = userData.ID
+	apiTest.InitializeServer(t.Server)
+	apiTest.SetHeader("Content-Type", "application/json")
+	body, err := apiTest.ReadRow(creadentials, nil, false)
+	if err != nil {
+		return err
+	}
+
+	apiTest.Body = body
+	apiTest.SendRequest()
+
+	// if err := apiTest.AssertStatusCode(http.StatusBadRequest); err != nil {
+	// 	return err
+	// }
+
+	err = json.Unmarshal(apiTest.ResponseBody, &t.response)
+	if err != nil {
+		return err
+	}
+
+	permission, err := apiTest.ReadCellString(creadentials, "role")
+	if err != nil {
+		return err
+	}
+	t.enforcer.AddRoleForUser(t.User.ID.String(), permission)
+	t.AccessToken = t.response.Data.AccessToken
+	return nil
 }
