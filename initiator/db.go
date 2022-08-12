8abb4317a9a7a6a470fa586bf18854a50e3c9967
yonas kemon <yonaskemon01@gmail.com>
2022-08-12 14:45:11 +0300

    fixed bug

--- initiator/db.go
+++ initiator/db.go
@@ -10,6 +10,9 @@ import (
 
 func InitDB(url string, log logger.Logger) *pgx.Conn {
 	config, err := pgx.ParseConfig(url)
+	if err != nil {
+		log.Fatal(context.Background(), fmt.Sprintf("Failed to connect to database: %v", err))
+	}
 	config.Logger = log
 	conn, err := pgx.ConnectConfig(context.Background(), config)
 	if err != nil {
