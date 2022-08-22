0e9a11f59a2d9d2f0a28521ef5d12665bccf8c10
Kirubel Adamu <kirubeladamu@gmail.com>
2022-08-22 14:27:17 +0300

    updated database connection to pgxpool

--- initiator/casbin.go
+++ initiator/casbin.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"fmt"
 	"github.com/casbin/casbin/v2"
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"sso/platform/logger"
 	"sso/platform/pgxadapter"
 )
 
-func InitEnforcer(path string, conn *pgx.Conn, log logger.Logger) *casbin.Enforcer {
-	adapter, err := pgxadapter.NewAdapterWithDB(conn)
+func InitEnforcer(path string, conn *pgxpool.Pool, log logger.Logger) *casbin.Enforcer {
+	pgxConn, err := conn.Acquire(context.Background())
+	if err != nil {
+		log.Fatal(context.Background(), fmt.Sprintf("Failed to connect to database: %v", err))
+	}
+	adapter, err := pgxadapter.NewAdapterWithDB(pgxConn.Conn())
 	if err != nil {
 		log.Fatal(context.Background(), fmt.Sprintf("Failed to create adapter: %v", err))
 	}
