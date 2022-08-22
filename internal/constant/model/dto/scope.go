88988ff719c213a638df61be00b52af05ffd22ed
Natnael Meresa <natnaelmeresa6@gmail.com>
2022-08-22 10:48:56 +0300

    SSO-7 modifies scopes model

--- internal/constant/model/dto/scope.go
+++ internal/constant/model/dto/scope.go
@@ -1,8 +1,19 @@
 package dto
 
+import validation "github.com/go-ozzo/ozzo-validation/v4"
+
 type Scope struct {
 	// The scope name.
 	Name string `json:"name,omitempty"`
 	// The scope description.
 	Description string `json:"description,omitempty"`
+	// resource server name
+	ResourceServerName string `json:"resource_server_name,omitempty"`
+}
+
+func (s Scope) Validate() error {
+	return validation.ValidateStruct(&s,
+		validation.Field(&s.Name, validation.Required.Error("name is required")),
+		validation.Field(&s.Description, validation.Required.Error("description is required")),
+	)
 }
