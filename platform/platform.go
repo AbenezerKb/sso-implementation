7efc2281eb190ba1c69e4124729c3dd17a1a88da
yonas kemon <yonaskemon01@gmail.com>
2022-08-26 11:07:04 +0300

    SSO-57 created function to Generate access token for Clients

--- platform/platform.go
+++ platform/platform.go
@@ -28,6 +28,7 @@ type SMSClient interface {
 
 type Token interface {
 	GenerateAccessToken(ctx context.Context, userID string, expiresAt time.Duration) (string, error)
+	GenerateAccessTokenForClient(ctx context.Context, userID, clientID, scope string, expiresAt time.Duration) (string, error)
 	GenerateRefreshToken(ctx context.Context) string
 	GenerateIdToken(ctx context.Context, user *dto.User) (string, error)
 	VerifyToken(signingMethod jwt.SigningMethod, token string) (bool, *jwt.RegisteredClaims)
