ff14e08d44a3a16217e469df9745724ad0a83983
Kirubel Adamu <kirubeladamu@gmail.com>
2022-09-28 15:12:07 +0300

    SSO-128 updated resource server model to include id,created and updated at times

--- internal/constant/model/dto/resource_server.go
+++ internal/constant/model/dto/resource_server.go
@@ -1,8 +1,19 @@
 package dto
 
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
 // ResourceServer is a server that this sso controls access for
 type ResourceServer struct {
+	// ID is the unique id for this resource server
+	ID uuid.UUID `json:"id"`
 	// Name is the resource server name.
 	// It must be unique across the sso
 	Name string `json:"name"`
+	// CreatedAt is the time this resource server is created at
+	CreatedAt time.Time `json:"created_at"`
+	// UpdatedAt is the time this resource server is updated at
+	UpdatedAt time.Time `json:"updated_at"`
 }
