4f2500bd9ea90262d40e68804bcd66efc84bc31d
Kirubel Adamu <kirubeladamu@gmail.com>
2022-09-30 09:14:53 +0300

    SSO-128 added resource server persistence to initiator

--- initiator/persistence.go
+++ initiator/persistence.go
@@ -7,6 +7,7 @@ import (
 	"sso/internal/storage/persistence/oauth"
 	"sso/internal/storage/persistence/oauth2"
 	"sso/internal/storage/persistence/profile"
+	"sso/internal/storage/persistence/resource-server"
 	"sso/internal/storage/persistence/scope"
 	"sso/internal/storage/persistence/user"
 	"sso/platform/logger"
@@ -14,21 +15,23 @@ import (
 
 type Persistence struct {
 	// TODO implement
-	OAuthPersistence   storage.OAuthPersistence
-	OAuth2Persistence  storage.OAuth2Persistence
-	ClientPersistence  storage.ClientPersistence
-	ScopePersistence   storage.ScopePersistence
-	UserPersistence    storage.UserPersistence
-	ProfilePersistence storage.ProfilePersistence
+	OAuthPersistence          storage.OAuthPersistence
+	OAuth2Persistence         storage.OAuth2Persistence
+	ClientPersistence         storage.ClientPersistence
+	ScopePersistence          storage.ScopePersistence
+	UserPersistence           storage.UserPersistence
+	ProfilePersistence        storage.ProfilePersistence
+	ResourceServerPersistence storage.ResourceServerPersistence
 }
 
 func InitPersistence(db persistencedb.PersistenceDB, log logger.Logger) Persistence {
 	return Persistence{
-		OAuthPersistence:   oauth.InitOAuth(log.Named("oauth-persistence"), db.Queries),
-		ClientPersistence:  client.InitClient(log.Named("client-persistence"), db.Queries),
-		OAuth2Persistence:  oauth2.InitOAuth2(log.Named("oauth2-persistence"), db.Queries),
-		ScopePersistence:   scope.InitScopePersistence(log.Named("scope-persistence"), db.Queries),
-		UserPersistence:    user.InitUserPersistence(log.Named("user-persistence"), db.Queries),
-		ProfilePersistence: profile.InitProfilePersistence(log.Named("profile-persistence"), db.Queries),
+		OAuthPersistence:          oauth.InitOAuth(log.Named("oauth-persistence"), db.Queries),
+		ClientPersistence:         client.InitClient(log.Named("client-persistence"), db.Queries),
+		OAuth2Persistence:         oauth2.InitOAuth2(log.Named("oauth2-persistence"), db.Queries),
+		ScopePersistence:          scope.InitScopePersistence(log.Named("scope-persistence"), db.Queries),
+		UserPersistence:           user.InitUserPersistence(log.Named("user-persistence"), db.Queries),
+		ProfilePersistence:        profile.InitProfilePersistence(log.Named("profile-persistence"), db.Queries),
+		ResourceServerPersistence: resource_server.InitResourceServerPersistence(log.Named("resource-server-persistence"), &db),
 	}
 }
