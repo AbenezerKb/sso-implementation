bb5e62ff0ed68bec5a13327537404b56e6223d56
Kirubel Adamu <kirubeladamu@gmail.com>
2022-09-30 10:37:02 +0300

    SSO-128 registered create resource server route

---
+++ internal/glue/routing/resource-server/resource_server.go
@@ -0,0 +1,29 @@
+package resource_server
+
+import (
+	"github.com/casbin/casbin/v2"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"sso/internal/constant/permissions"
+	"sso/internal/glue/routing"
+	"sso/internal/handler/middleware"
+	"sso/internal/handler/rest"
+)
+
+func InitRoute(group *gin.RouterGroup, resourceServer rest.ResourceServer, authMiddleware middleware.AuthMiddleware, enforcer *casbin.Enforcer) {
+	resourceServers := group.Group("/resourceServer")
+	resourceServerRoutes := []routing.Router{
+		{
+			Method:  http.MethodPost,
+			Path:    "",
+			Handler: resourceServer.CreateResourceServer,
+			Middlewares: []gin.HandlerFunc{
+				authMiddleware.Authentication(),
+				authMiddleware.AccessControl(),
+			},
+			Permission: permissions.CreateResourceServer,
+		},
+	}
+
+	routing.RegisterRoutes(resourceServers, resourceServerRoutes, enforcer)
+}
