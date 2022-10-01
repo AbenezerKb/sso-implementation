8a83644978586cfe491ef2ff9824620bd1306f43
Natnael Meresa <natnaelmeresa6@gmail.com>
2022-10-01 08:46:14 +0300

    SSO-125 add module for mini_ride check phone

--- internal/module/module.go
+++ internal/module/module.go
@@ -60,4 +60,5 @@ type ProfileModule interface {
 type MiniRideModule interface {
 	ListenMiniRideEvent(ctx context.Context)
 	ProcessEvents(ctx context.Context, miniRideEvent *request_models.MinRideEvent, wg *sync.WaitGroup)
+	CheckPhone(ctx context.Context, phone string) (*dto.MiniRideResponse, error)
 }
