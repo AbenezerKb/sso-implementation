081e5f6e4ba8c1bdb292e350f88add8d8ad2b7a2
Kirubel Adamu <kirubeladamu@gmail.com>
2022-10-03 12:05:34 +0300

    SSO-136 added get permissions queries

---
+++ internal/constant/model/persistencedb/permission.go
@@ -0,0 +1,46 @@
+package persistencedb
+
+import (
+	"context"
+	"sso/internal/constant/permissions"
+)
+
+const getAllPermissions = "SELECT * FROM casbin_rule WHERE p_type = 'p'"
+
+func (db *PersistenceDB) GetAllPermissions(ctx context.Context) ([]permissions.Permission, error) {
+	rows, err := db.pool.Query(ctx, getAllPermissions)
+	if err != nil {
+		return nil, err
+	}
+
+	var perms []permissions.Permission
+	for rows.Next() {
+		var i permissions.Permission
+		if err := rows.Scan(nil, nil, &i.ID, &i.Name, &i.Category); err != nil {
+			return nil, err
+		}
+		perms = append(perms, i)
+	}
+
+	return perms, nil
+}
+
+const getPermissionsOfCategory = "SELECT * FROM casbin_rule WHERE p_type = 'p' AND v2 = $1"
+
+func (db *PersistenceDB) GetPermissionsOfCategory(ctx context.Context, category string) ([]permissions.Permission, error) {
+	rows, err := db.pool.Query(ctx, getPermissionsOfCategory, category)
+	if err != nil {
+		return nil, err
+	}
+
+	var perms []permissions.Permission
+	for rows.Next() {
+		var i permissions.Permission
+		if err := rows.Scan(nil, nil, &i.ID, &i.Name, &i.Category); err != nil {
+			return nil, err
+		}
+		perms = append(perms, i)
+	}
+
+	return perms, nil
+}
