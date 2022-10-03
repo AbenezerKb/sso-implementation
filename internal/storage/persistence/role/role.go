ca9d698283510a46506d397f932f4ecd7e54996f
Kirubel Adamu <kirubeladamu@gmail.com>
2022-10-03 13:48:22 +0300

    SSO-136 updated get permissions persistence to include errorx

--- internal/storage/persistence/role/role.go
+++ internal/storage/persistence/role/role.go
@@ -2,6 +2,10 @@ package role
 
 import (
 	"context"
+	"fmt"
+	"go.uber.org/zap"
+	"sso/internal/constant/errors"
+	"sso/internal/constant/errors/sqlcerr"
 	"sso/internal/constant/model/persistencedb"
 	"sso/internal/constant/permissions"
 	"sso/internal/storage"
@@ -22,7 +26,24 @@ func InitRolePersistence(logger logger.Logger, db *persistencedb.PersistenceDB) 
 
 func (r *rolePersistence) GetAllPermissions(ctx context.Context, category string) ([]permissions.Permission, error) {
 	if category == "" {
-		return r.db.GetAllPermissions(ctx)
+		perms, err := r.db.GetAllPermissions(ctx)
+		if err != nil {
+			err := errors.ErrReadError.Wrap(err, "error reading permissions")
+			r.logger.Error(ctx, "unable to read permissions", zap.Error(err))
+			return nil, err
+		}
+		return perms, nil
 	}
-	return r.db.GetPermissionsOfCategory(ctx, category)
+	perms, err := r.db.GetPermissionsOfCategory(ctx, category)
+	if err != nil {
+		if sqlcerr.Is(err, sqlcerr.ErrNoRows) {
+			err = errors.ErrInvalidUserInput.Wrap(err, fmt.Sprintf("category %s doesn't exist", category))
+			r.logger.Info(ctx, "category was not found", zap.String("category", category), zap.Error(err))
+			return nil, err
+		}
+		err := errors.ErrReadError.Wrap(err, "error reading permissions", zap.String("category", category))
+		r.logger.Error(ctx, "unable to read permissions", zap.Error(err), zap.String("category", category))
+		return nil, err
+	}
+	return perms, nil
 }
