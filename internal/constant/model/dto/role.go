060a30f726c4fa13600c2f3146bf2627521613ea
Kirubel Adamu <kirubeladamu@gmail.com>
2022-10-04 09:25:24 +0300

    SSO-140 updated role model to only contain permission ids

--- internal/constant/model/dto/role.go
+++ internal/constant/model/dto/role.go
@@ -1,13 +1,9 @@
 package dto
 
-import (
-	"sso/internal/constant/permissions"
-)
-
 // Role is a set of defined permissions that are grouped together with a name
 type Role struct {
 	// Name is a unique name for the role
 	Name string `json:"name"`
-	// Permissions are the list of permissions this role contains
-	Permissions []permissions.Permission `json:"permissions"`
+	// Permissions are the list of permissions names this role contains
+	Permissions []string `json:"permissions"`
 }
