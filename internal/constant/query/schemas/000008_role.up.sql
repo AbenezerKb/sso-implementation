8d8964d6e2a942c655d8a08d58d8472137b14b59
Kirubel Adamu <kirubeladamu@gmail.com>
2022-10-04 16:07:57 +0300

    SSO-140 updated roles table to have default status value

--- internal/constant/query/schemas/000008_role.up.sql
+++ internal/constant/query/schemas/000008_role.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE roles
 (
     name       VARCHAR(255) PRIMARY KEY,
-    status     VARCHAR(255),
+    status     VARCHAR(255) DEFAULT 'ACTIVE',
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now()
 );
