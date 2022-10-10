987dc67d3e4b44d6066f6502463429c97a227881
Kirubel Adamu <kirubeladamu@gmail.com>
2022-10-10 10:44:55 +0300

    SSO-164 updated scope validation to check for whitespaces

--- internal/constant/model/dto/scope.go
+++ internal/constant/model/dto/scope.go
@@ -1,6 +1,7 @@
 package dto
 
 import (
+	"fmt"
 	"time"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
@@ -19,9 +20,22 @@ type Scope struct {
 
 func (s Scope) Validate() error {
 	return validation.ValidateStruct(&s,
-		validation.Field(&s.Name, validation.Required.Error("name is required")),
+		validation.Field(&s.Name, validation.Required.Error("name is required"), validation.By(noSpaceScope)),
 		validation.Field(&s.Description, validation.Required.Error("description is required")),
 	)
+}
+func noSpaceScope(value interface{}) error {
+	scope, ok := value.(string)
+	if !ok {
+		return fmt.Errorf("invalid value for scope")
+	}
+	scopeRunes := []rune(scope)
+	for i := 0; i < len(scopeRunes); i++ {
+		if scopeRunes[i] == ' ' {
+			return fmt.Errorf("name can not contain spaces")
+		}
+	}
+	return nil
 }
 
 type UpdateScopeParam struct {
