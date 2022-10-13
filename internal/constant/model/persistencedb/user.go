c673c1d5084e889198ef6fd27e97846cccfd8c7a
Kirubel Adamu <kirubeladamu@gmail.com>
2022-10-13 17:05:32 +0300

    SSO-189 added new query to get users list with role

---
+++ internal/constant/model/persistencedb/user.go
@@ -0,0 +1,47 @@
+package persistencedb
+
+import (
+	"context"
+	"database/sql"
+	"sso/internal/constant/model/dto"
+	"sso/platform/utils"
+)
+
+func (db *PersistenceDB) GetAllUsersWithRole(ctx context.Context, pgnFlt string) ([]dto.User, int, error) {
+	rows, err := db.pool.Query(ctx, utils.ComposeFullFilterSQL(ctx, "user_role", pgnFlt))
+	if err != nil {
+		return nil, 0, err
+	}
+	defer rows.Close()
+
+	var users []dto.User
+	var totalCount int
+	for rows.Next() {
+		var i dto.User
+		var email, profilePicture, status sql.NullString
+		if err := rows.Scan(
+			&i.ID,
+			&i.FirstName,
+			&i.MiddleName,
+			&i.LastName,
+			&email,
+			&i.Phone,
+			&i.Gender,
+			&profilePicture,
+			&status,
+			&i.CreatedAt,
+			&i.Role,
+			&totalCount); err != nil {
+			return nil, 0, err
+		}
+		i.Email = email.String
+		i.ProfilePicture = profilePicture.String
+		i.Status = status.String
+		users = append(users, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, 0, err
+	}
+
+	return users, totalCount, nil
+}
