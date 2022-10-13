b96fe53ccf2c7ed1fb88121b1ac98716a6033052
Natnael Meresa <natnaelmeresa6@gmail.com>
2022-10-13 17:06:49 +0300

    SSO-187 modify sqlc to add user agent and ip

--- internal/constant/query/schemas/000006_internalrefreshtoken.up.sql
+++ internal/constant/query/schemas/000006_internalrefreshtoken.up.sql
@@ -1,7 +1,9 @@
 CREATE TABLE internalrefreshtokens (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    refreshtoken varchar(255) NOT NULL,
+    refresh_token varchar(255) NOT NULL,
     user_id UUID NOT NULL,
+    ip_address varchar NOT NULL,
+    user_agent varchar NOT NULL,
     expires_at timestamptz NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now(),
