723b0ba246f7110013f5385047eb827953daa8a7
Kirubel Adamu <kirubeladamu@gmail.com>
2022-11-25 15:15:00 +0300

    SSO-212 updated handler

--- internal/handler/rest/rs-api/rs_api.go
+++ internal/handler/rest/rs-api/rs_api.go
@@ -30,8 +30,7 @@ func Init(logger logger.Logger, rs module.RSAPI) rest.RSAPI {
 // @Tags         internal
 // @Accept       json
 // @Produce      json
-// @param id query string true "id"
-// @param phone query string true "phone"
+// @param user query request_models.RSAPIUserRequest true "user"
 // @Success      200  {object}  dto.User
 // @Failure      400  {object}  model.ErrorResponse
 // @Router       /internal/user [get]
@@ -54,5 +53,37 @@ func (i *rsAPI) GetUserByPhoneOrID(ctx *gin.Context) {
 	}
 
 	i.logger.Info(ctx, "user details fetched", zap.Any("request", req))
+	constant.SuccessResponse(ctx, http.StatusOK, user, nil)
+}
+
+// GetUsersByPhoneOrID	 returns users with the specified ids or phones.
+// @Summary      returns users with ids or phone numbers
+// @Description  returns user with ids or phone numbers.
+// @Tags         internal
+// @Accept       json
+// @Produce      json
+// @param users body request_models.RSAPIUsersRequest true "users"
+// @Success      200  {object}  []dto.User
+// @Failure      400  {object}  model.ErrorResponse
+// @Router       /internal/users [get]
+// @Security	BasicAuth
+func (i *rsAPI) GetUsersByPhoneOrID(ctx *gin.Context) {
+	var req request_models.RSAPIUsersRequest
+	err := ctx.ShouldBind(&req)
+	if err != nil {
+		err := errors.ErrInvalidUserInput.Wrap(err, "invalid request body")
+		i.logger.Info(ctx, "invalid request body for rs-api")
+		_ = ctx.Error(err)
+		return
+	}
+
+	requestCtx := ctx.Request.Context()
+	user, err := i.rsAPI.GetUsersByIDOrPhone(requestCtx, req)
+	if err != nil {
+		_ = ctx.Error(err)
+		return
+	}
+
+	i.logger.Info(ctx, "users detail fetched")
 	constant.SuccessResponse(ctx, http.StatusOK, user, nil)
 }
